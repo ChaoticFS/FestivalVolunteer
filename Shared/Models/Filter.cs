28cbaa238d42e0cc080f8b3bebd12a640120312a
Magnus Overgård <magnus.overgaard1711@gmail.com>
2022-12-08 14:49:04 +0100

    Added filter object for shift filtering

---
+++ Shared/Models/Filter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestivalVolunteer.Shared.Models
+{
+    public class Filter
+    {
+        public DateTime? Date { get; set; }
+        public string? Area { get; set; }
+        public int? VolunteersNeeded { get; set; }
+        public int? Priority { get; set; }
+        public bool? Locked { get; set; }
+    }
+}
