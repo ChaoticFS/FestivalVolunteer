725f255afee50c914d81aafbe934d41314cb4da8
Magnus Overgård <magnus.overgaard1711@gmail.com>
2022-12-08 12:17:48 +0100

    Throws notimplementedexceptions

--- Server/Models/ShiftRepository.cs
+++ Server/Models/ShiftRepository.cs
@@ -5,5 +5,26 @@ namespace FestivalVolunteer.Server.Models
     internal class ShiftRepository : IShiftRepository
     {
         DBContext dbShift = new DBContext();
+
+        public List<Shift> GetFilteredShifts()
+        {
+            throw new NotImplementedException();
+        }
+        public Shift GetShift(string id)
+        {
+            throw new NotImplementedException();
+        }
+        public void PostShift(Shift shift)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteShift(string id)
+        {
+            throw new NotImplementedException();
+        }
+        public void PutShift(Shift shift)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
