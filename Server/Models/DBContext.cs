b1148b47b550af6f61240c4e29b9b1b4dd5a1208
Magnus Overgård <magnus.overgaard1711@gmail.com>
2022-12-12 21:59:11 +0100

    db connection established

--- Server/Models/DBContext.cs
+++ Server/Models/DBContext.cs
@@ -1,7 +1,14 @@
-﻿namespace FestivalVolunteer.Server.Models
+﻿using Npgsql;
+
+namespace FestivalVolunteer.Server.Models
 {
     public class DBContext
     {
-
+        public readonly NpgsqlConnection conn;
+        public DBContext()
+        {
+            string connString = "User ID=starfest;Password={password};Host=starfest.postgres.database.azure.com;Port=5432;Database=starfest";
+            conn = new NpgsqlConnection(connString);
+        }
     }
 }
--- Server/Models/TeamRepository.cs
+++ Server/Models/TeamRepository.cs
@@ -1,20 +1,29 @@
-﻿using FestivalVolunteer.Shared.Models;
+﻿using Dapper;
+using FestivalVolunteer.Shared.Models;
 
 namespace FestivalVolunteer.Server.Models
 {
     public class TeamRepository : ITeamRepository
     {
+        DBContext db = new DBContext();
         public List<User> GetTeamMembers(int teamid)
         {
             throw new NotImplementedException();
         }
         public Team GetTeam(int teamid)
         {
-            throw new NotImplementedException();
+            var sql = $"SELECT name " +
+                      $"FROM team " +
+                      $"WHERE team_id={teamid}";
+
+            return db.conn.Query<Team>(sql).First();
         }
         public void PostTeam(Team team)
         {
-            throw new NotImplementedException();
+            var sql = $"INSERT INTO team (name)" +
+                      $"VALUES ('{team.Name}')";
+
+            db.conn.Query(sql);
         }
         public void DeleteTeam(int teamid)
         {
